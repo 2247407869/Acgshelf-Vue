65c5d8934eb04043f15dc59b891d9eb7059539ce
lls-laptop <2247407869@qq.com>
2018-08-07 14:50:56 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+
+      </el-header>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.el-header, .el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <div class="ranking">
+    <li v-for="anime in animes">
+      {{ anime.name }}
+    </li>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      animes:[],
+      url_params: {
+        pageNum:'1',
+        pageSize:'30'
+      },
+    }
+  },
+  created: function() {
+    this.$http.get('http://localhost:8080/listCategory',{params:this.url_params})
+      .then((response) => {
+      this.animes = response.data.list;
+    });
+  },
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
